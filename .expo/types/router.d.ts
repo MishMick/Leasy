e846933637a648a4fc908c2ba71a12f01ead96a3
Mishal <mish95shah@gmail.com>
2024-11-11 22:48:28 -0500

    updates

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/index` | `/(tabs)/account` | `/(tabs)/create` | `/(tabs)/messages` | `/(tabs)/search` | `/_sitemap` | `/account` | `/create` | `/messages` | `/modal` | `/search`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/account` | `/(tabs)/create` | `/(tabs)/messages` | `/(tabs)/search` | `/_sitemap` | `/account` | `/create` | `/messages` | `/modal` | `/search`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
