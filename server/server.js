37cbece08b64bcaacf9d69441d221388942898e9
Mishal <mish95shah@gmail.com>
2025-03-01 21:58:40 -0500

    server setup enhanced

--- server/server.js
+++ server/server.js
@@ -1,33 +1,77 @@
+import express from 'express';
+import cors from 'cors';
+import { supabase } from './supabase.js';
+
 // server/server.js
-const express = require('express');
-const cors = require('cors');
+
 const app = express();
 const port = 3000;
-
 // Middleware
 app.use(cors()); // Allow cross-origin requests from React Native
 app.use(express.json()); // Parse JSON bodies
 
-const listings = require('../data/listings.json');
+//const listings = require('../data/listings.json');
 
-// Sample API endpoint
+const fetchListings = async () => {
+  const { data, error } = await supabase.from('listings').select('*');
+  
+  if (error) console.error(error);
+  return data;
+};
+
+const createListing = async (listing) => {
+  const { data, error } = await supabase.from('listings').insert([listing]);
+
+  if (error) console.error(error);
+  return data;
+};
+
+const fetchListingById = async (id) => {
+  const { data, error } = await supabase
+    .from('listings')
+    .select('*')
+    .eq('id', id)
+    .single(); // Ensures only one record is returned
+
+  if (error) {
+    console.error('Error fetching listing:', error);
+    return null;
+  }
+  return data;
+};
+
+// GET listings API endpoint
 app.get('/api/listings', (req, res) => {
-  res.json({
-    result: listings,
-    success: true,
-  });
+  fetchListings()
+    .then(listings => {
+      console.log("Listings ", listings);
+      res.json({
+        result: listings,
+        success: true,
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching listings:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
-// Sample API endpoint
+// Get listing by id API endpoint
 app.get('/api/listings/:id', (req, res) => {
   const itemId = req.params.id;
-  const item = listings.find(item => item.id === parseInt(itemId));
 
-  if (item) {
-    res.json(item);
-  } else {
-    res.status(404).json({ error: 'Item not found' });
-  }
+  fetchListingById(itemId)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        res.status(404).json({ error: 'Item not found' });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching listing:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
 // Start the server
